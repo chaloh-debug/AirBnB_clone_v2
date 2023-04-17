629802a23533b952090d0349f3625ea04cf02e31
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-18 02:27:36 +0300

    database and user create

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,5 +2,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
 GRANT ALL PRIVILEDGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT PRIVILEDGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEDGES;
