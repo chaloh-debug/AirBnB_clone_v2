04c923135cdfaa50024ea4d79c5d063f7c9e64f4
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-18 00:46:29 +0300

    database and user create

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Setup a basic MySQL server
+-- creating database and user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
