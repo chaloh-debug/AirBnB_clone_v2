5fd1d0a4e6c7bacbb1800182385a5084f419f461
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-19 13:54:06 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -6,11 +6,9 @@ from sqlalchemy.orm import relationship
 from models.place import place_amenity
 from os import getenv
 
+
 class Amenity(BaseModel, Base):
     """ amenities inluded"""
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        places_amenities = relationship("Place", secondary=place_amenity)
-    else:
-        name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    places_amenities = relationship("Place", secondary=place_amenity)
--- models/city.py
+++ models/city.py
@@ -9,15 +9,9 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-
-        places = relationship("Place", backref="cities",
-                              cascade='all, delete, delete-orphan')
-
-    else:
-        state_id = ""
-        name = ""
+    places = relationship("Place", backref="cities",
+                          cascade='all, delete, delete-orphan')
