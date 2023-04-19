4157dc7b675491d85c47d0a7b20723a1748dbd6f
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-19 11:04:36 +0300

    properties update

--- models/state.py
+++ models/state.py
@@ -2,9 +2,24 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from sqlalchemy import String, Column, Integer
+from os import getenv
+from sqlalchemy.orm import relationship
+from model.city import City
+import models
 
 
 class State(BaseModel):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade='all, delete, delete-orphan')
+
+    def cities(self):
+        """returns a list of city elements"""
+        dct = []
+        val = models.storage.all(City)
+        for i in list(val.values()):
+            if i.state_id == self.id:
+                dct.append(i)
+        return dct
