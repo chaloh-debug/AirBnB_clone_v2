783c2e268efab411d07edcc0610f736fade33acf
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-19 11:05:52 +0300

    properties update

--- models/state.py
+++ models/state.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 from sqlalchemy import String, Column, Integer
 from os import getenv
 from sqlalchemy.orm import relationship
-from model.city import City
+from models.city import City
 import models
 
 
