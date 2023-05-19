94a5464f95eb2bf7d325c372596c7008e285fce9
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-19 17:57:15 +0300

    update

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_hbnb():
+    """
+    return hello hbnb
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    returns hbnb
+    """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    display “C ” followed by the value
+    of the text variable 
+    """
+    return "C {}".format(text).replace('_',' ')
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_text(text="is cool"):
+    """
+    display “Python ”, followed by the
+    value of the text variable 
+    """
+    return "Python {}".format(text).replace('_',' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_hbnb():
+    """
+    return hello hbnb
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    returns hbnb
+    """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    display “C ” followed by the value
+    of the text variable 
+    """
+    return "C {}".format(text).replace('_',' ')
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_text(text="is cool"):
+    """
+    display “Python ”, followed by the
+    value of the text variable 
+    """
+    return "Python {}".forma(text).replace('_',' ')
+
+@app.route('/number/<int:n>')
+def n_int(n):
+    """
+    display “n is a number” only if
+    n is an integer
+    """
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>')
+def n_template(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('templates/5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
