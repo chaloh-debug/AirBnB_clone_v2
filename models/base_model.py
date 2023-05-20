c988574c658e476bab3cd0550566df9bf3c34256
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-20 19:35:10 +0300

    base_model fix

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
                 if k != "__class__":
                     setattr(self, k, v)
         self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        self.updated_at = self.created_at = datetime.now()
 
     def __str__(self):
         """returns a string
