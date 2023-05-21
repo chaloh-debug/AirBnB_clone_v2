3926d5fcd83654a7d425dd80c89aa3da860fd499
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-21 16:11:45 +0300

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,90 @@
 #!/usr/bin/python3
 """
-Flask web application                           """
-from flask import Flask, render_template
+Flask web application
+"""
+from flask import Flask, render_template, escape
 from models import storage
 from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/state_list')
-def store():
+@app.route('/')
+def hello_hbnb():
     """
-    Display states sorted by name
+    return hello hbnb
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return "Hello HBNB!"
 
 
-@app.teardown_appcontent
-def teardown(exception):
+@app.route('/hbnb')
+def hbnb():
     """
-    remove the current SQLAlchemy Session
+    returns hbnb
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    display “C ” followed by the value
+    of the text variable
+    """
+    return "C {}".format(text).replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_text(text="is cool"):
+    """
+    display “Python ”, followed by the
+    value of the text variable
+    """
+    return "Python {}".forma(text).replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def n_int(n):
+    """
+    display “n is a number” only if
+    n is an integer
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def n_template(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_or_even(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route('/states_list')
+def states_list():
+    """
+    lists available states
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """
+    closes current sqlalchemy session
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,61 +1,122 @@
 #!/usr/bin/python3
 """
-Flask web application                           """
-from flask import Flask, render_template
+Flask web application
+"""
+from flask import Flask, render_template, escape
 from models import storage
 from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/state_list')
-def state_list():
+@app.route('/')
+def hello_hbnb():
     """
-    Display states sorted by name
+    return hello hbnb
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    return "Hello HBNB!"
 
 
-@app.route('cities_by_states')
+@app.route('/hbnb')
+def hbnb():
+    """
+    returns hbnb
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    display “C ” followed by the value
+    of the text variable
+    """
+    return "C {}".format(text).replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_text(text="is cool"):
+    """
+    display “Python ”, followed by the
+    value of the text variable
+    """
+    return "Python {}".forma(text).replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def n_int(n):
+    """
+    display “n is a number” only if
+    n is an integer
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def n_template(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_or_even(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route('/states_list')
+def states_list():
+    """
+    Displays available states
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states')
 def cities_by_states():
     """
-    Display list of Cities
+    Displays available states
     """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.route('/states')
 @app.route('/states/<id>')
 def states_by_id(id=None):
     """
-    Display cities by their id and cities
-    or diplay states only
+    Displays states by there id and there cities
+    or just states
     """
-    states = storage.all("State")
-    if id is None:
-        case = 1
-        state = None
-    else:
-        by_id = "State." + str(id)
-        if by_id in states.keys():
+    states = storage.all(State)
+    if id is not None:
+        state_id = 'State.' + str(id)
+        if state_id in states.keys():
             state = states[state_id]
             case = 2
         else:
             case = 3
             state = None
+    else:
+        case = 1
+        state = None
     return render_template('9-states.html', state=state,
                            states=states.values(), case=case)
 
 
-@app.teardown_appcontent
-def teardown(exception):
+@app.teardown_appcontext
+def teardown_app(exception):
     """
-    remove the current SQLAlchemy Session
+    closes current sqlalchemy session
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
